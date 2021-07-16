7ab426408e7f0cf4c695770f3ea36408caa7484f
chippysaji <sajichippy16@gmail.com>
2021-07-16 07:39:14 +0530

    java file adding

---
+++ Even.java
@@ -0,0 +1,15 @@
+class Even
+{
+public static void main (String args[])
+{
+System.out.println("even number");
+System.out.println();
+for(int i=1;i<=10;i++)
+{
+
+if(i%2==1)
+   continue;
+System.out.println(i);
+}
+}
+}
