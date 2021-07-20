52d6b71dd68f794078f3f730b5a58717fe452893
chippysaji <87402429+chippysaji@users.noreply.github.com>
2021-07-20 16:11:28 +0530

    Update Even.java

--- Even.java
+++ Even.java
@@ -2,7 +2,7 @@ class Even
 {
 public static void main (String args[])
 {
-System.out.println("even number training");
+System.out.println("even number is:");
 System.out.println();
 for(int i=1;i<=10;i++)
 {
